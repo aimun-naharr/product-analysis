8450693f434cf7d918965e41a7db5b2f52c1a74a
aimunhidden485 <aimunnaharr@gmail.com>
2022-04-03 21:13:03 +0600

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Components/Blogs/Blogs';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Home from './Components/Home/Home';
+import NotFound from './Components/Notfound/NotFound';
+import Reviews from './Components/Reviews/Reviews';
 
 function App() {
   return (
     <div>
-      <h1>hi</h1>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/reviews' element={<Reviews/>}></Route>
+        <Route path='/Blogs' element={<Blogs/>}></Route>
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
+        <Route path='*' element={<NotFound/>}></Route>
+      </Routes>
     </div>
   );
 }
